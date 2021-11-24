173537280ced9b16ed7256a64a0a748d41c3f523
Rachel Malone <rachel.k.malone@accenture.com>
2021-11-24 19:08:14 +0000

    Added function solve_2204b7a8(x) and make changes to solve_ed36ccf7(x) doc string

--- src/manual_solve.py
+++ src/manual_solve.py
@@ -11,13 +11,15 @@ import re
 
 def solve_ed36ccf7(x):
     
-    """ This function takes the input square array x and then
+    """ This function takes the input array x and then
     rotates the array by 90 degrees anti-clockwise. This
     rotated array is then outputted as the result and
     is called x_out.
 
-    Keyword arguments:
+    Parameters:
     x -- input array
+    
+    Returns:
     x_out -- output array
     """
     
@@ -34,7 +36,63 @@ def solve_ed36ccf7(x):
     
     return x_out
 
-def solve_b2862040(x):
+def solve_2204b7a8(x):
+
+    """ This function takes the input array x and searches for the non-zero
+    boundary lines. These boundaries will exist on the top and bottom rows or
+    on the left most and irght most column. Any remaining non-zero values will be
+    mapped to these boundary values based on proximity (value is mapped to
+    boundary value nearest to it)
+
+    Parameters:
+    x -- input array
+    
+    Returns:
+    x -- array after values have been mapped
+    """
+    
+    ### Find the 2 colours to map to
+    ### Check if they are on the left most/ Right most vertical axis
+    ### Check the colour of the midpoint of the left column isn't 0 or 3
+    if x[x.shape[0]//2, 0] != 0 and x[x.shape[0]//2, 0] != 3:
+        vert_1 = x[x.shape[0]//2, 0]
+        vert_2 = x[x.shape[0]//2, x.shape[1] - 1]
+        
+        ### Iterate through the values of x and check where there's a 3.
+        for i in range(x.shape[0] - 1):
+            for j in range(x.shape[1] - 1):
+                
+                ### Check if this value's column index is less than the middle index.
+                ### If both true, map this value to colour_1_vert.
+                if x[i,j] == 3 and j < x.shape[1] // 2:
+                    x[i,j] = vert_1
+                
+                ### If value == 3 but column index is greater than or equal
+                ### to middle index, map this value to colour_2_vert.
+                elif x[i,j] == 3 and j >= x.shape[1] // 2:
+                    x[i,j] = vert_2
+        
+    ### Check if mapping colours are on top/ bottom horizontal axis
+    ### Check the colour of the midpoint of top row isn't 0 or 3
+    elif x[0, x.shape[1]//2] != 0 and x[0, x.shape[1]//2] != 3:
+        horiz_1 = x[0, x.shape[1] // 2]
+        horiz_2 = x[x.shape[0] - 1, x.shape[1]//2]
+        
+        
+        ### Iterate through the values of x and check where there's a 3.
+        for i in range(x.shape[0] - 1):
+            for j in range(x.shape[1] - 1):
+                
+                ### Check if this value's row index is less than the middle index.
+                ### If both true, map this value to colour_1_horiz.
+                if x[i,j] == 3 and i < x.shape[0] // 2:
+                    x[i,j] = horiz_1
+                    
+                ### If value == 3 but row index is greater than or equal
+                ### to middle index, map this value to colour_2_horiz.
+                elif x[i,j] == 3 and i >= x.shape[0] // 2:
+                    x[i,j] = horiz_2
+
     return x
 
 def solve_05269061(x):
