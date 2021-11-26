Fixed final line before return in function solve_a78176bb(x) as np.where() parameters had been entered in the incorrect order

src/manual_solve.py
@@ -357,7 +357,7 @@ def solve_a78176bb(x):
                 
                 
     ### Remove all values that are equal to 5
-    x_out = np.where(x_out == 5, 0, x_out)
+    x_out = np.where(x_out != 5, x_out, 0)
     return x_out
     
     